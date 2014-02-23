57ac4d52746dc9f8703d8d97834dc3f9436d391e
vineel <vineelkpratap@gmail.com>
2014-02-22 19:05:36 -0800

    vectorized implementation

--- helper.py
+++ helper.py
@@ -1,60 +1,59 @@
 import numpy as np
 from scipy import io as sio
-def perform_lda(X, K, alpha, beta, num_epochs):
-	print "LDA started...."
-	it = 0  #iterations
-	(M, V) = X.shape
-	Z = np.random.randint(K, size=X.sum()) #Z is defined for the whole document
-	(q,n) = load(X,K,V,Z,M)
-	sum_q = np.sum(q, axis=0)
-	sum_n = np.sum(n, axis=1)
-	sum_alpha = np.sum(alpha)
-	sum_beta = np.sum(beta)
+
+def perform_lda(X, K, M, V, alpha, beta, num_epochs):
+	
+############ initialisation ###########################################	
+	
+	Z = [] #topics of words of documents
+	q = np.zeros((K,V))+ beta #numerator term
+	n = np.zeros((M,K))+ alpha #numerator term
+	doc = []
+	
+	for m in range(M):
+		Z_doc = np.random.randint(K, size=X[m,:].sum())		#can do smart initialisation
+		nz =  X[m,:].nonzero()[1]
+		doc.append([nz[temp] for temp in range(nz.size) for temp2 in range(int(X[m,nz[temp]]))])
+		for i, w_i in enumerate(doc[m]):
+			z = Z_doc[i]
+			q[z,w_i],n[m,z] = q[z,w_i]+1,n[m,z]+1
+		Z.append(Z_doc)
+	Q = np.sum(q,axis=1) + np.sum(beta) #denominator term
 	p = np.empty(K, dtype=float)
-	while(it<num_epochs):
-		print "Iteration #" ,it+1, " of ", num_epochs
-		i = 0 #denotes the position of the word
+
+
+############ inference #############################################	
+	 
+	it = 0  #iterations
+	while(it < num_epochs):
+		
+		print "epoch #" ,it+1, " of ", num_epochs
 		for m in range(M):
-			nz =  X[m,:].nonzero()[1]
-			for w_i in ([nz[temp] for temp in range(nz.size) for temp2 in range(int(X[m,nz[temp]]))]):
-				for j in range(K):
-					err = -1 if Z[i]==j else 0
-					q_bar,n_bar = q[j,w_i] + err, n[m,j] + err
-					p[j] = (q_bar+beta[w_i])/(sum_q[j]+err+sum_beta) * (n_bar+alpha[j])	/(sum_n[m]+err+sum_alpha)
-				p =  p/(np.sum(p))
-				pre = Z[i]
-				cur = Z[i] = int(np.random.choice(K,1,p=p)) #randomly sample and update Z[i] based on p
-				#update q, sum_q, n, sum_n appropriately 
-				q[pre,w_i], q[cur,w_i] = q[pre,w_i]-1, q[cur,w_i]+1
-				sum_q[pre],sum_q[cur] = sum_q[pre]-1,sum_q[cur]+1
-				n[m,pre], n[m,cur] = n[m,pre]-1, n[m,cur]+1  
-				i+=1  #increment i with every occurance of word
+			for i, w_i in enumerate(doc[m]):
+				
+				#discount for i-th word w_i with topic z
+				z = Z[m][i]
+				q[z,w_i], n[m,z], Q[z] = q[z,w_i]-1, n[m,z]-1, Q[z]-1
+				
+				# sampling topic new_z for w_i
+				p = q[:,w_i] * n[m,:]  / Q
+				new_z = np.random.multinomial(1, p / p.sum()).argmax()
+				
+				# set z the new topic and increment counters
+				Z[m][i] = new_z
+				q[new_z,w_i], n[m,new_z], Q[new_z] = q[new_z,w_i]+1, n[m,new_z]+1, Q[new_z]+1
+				
 		it+=1
-	print "finished ..."
+	print "lda finished ..."
 	return Z
 
 
-def load (X, K, V, Z, M):
-	q = np.zeros((K,V), dtype=int)
-	n = np.zeros((M,K), dtype=int)
-	i=0
-	for m in range(M):
-		nz =  X[m,:].nonzero()[1]
-		for w_i in ([nz[temp] for temp in range(nz.size) for temp2 in range(int(X[m,nz[temp]]))]):
-			q[Z[i],w_i]+=1
-			n[m,Z[i]]+=1
-			i+=1
-	return (q,n)
-
-def predict(Z, K, doc_len):
-	print doc_len.shape
-	strt = 0
+def predict(Z, K, M):
 	count = np.empty(K, dtype=int)
-	y = np.empty(doc_len.size, dtype=int)
-	for i in range(doc_len.size):
-		end = strt + int(doc_len[i])-1;
+	y = np.empty(len(Z), dtype=int)
+	compare = [1..K]
+	for m in range(M):
 		for j in range(K):
-			count[j] = np.sum(Z[strt:end] == j)
-		y[i] = np.argmax(count) + 1
-		strt = end+1
+			count[j] = np.sum(Z[m] == j)
+		y[m] = np.argmax(count) + 1
 	return y
--- main.py
+++ main.py
@@ -4,10 +4,14 @@ f = sio.loadmat('classic400.mat', squeeze_me=True, struct_as_record=False)
 vocab =  f['classicwordlist']
 Y = np.array(f['truelabels'])
 X = f['classic400'] #scipy.sparse.csc_matrix
-K = 3 #no. of topics
-a = np.ones(K, dtype=float) #alpha
-b = np.ones(len(vocab), dtype=float) #beta
-Z = perform_lda(X, K, a, b, 30) #infers z
-Y_pred = predict(Z, K, X.sum(axis=1))
+
+
+K,(M,V) = 3,X.shape #no. of topics
+a = (50.0/K)*np.ones(K) #alpha - parameter of topics prio
+b = 0.1*np.ones(V) #beta -  parameter of words prior
+
+
+Z = perform_lda(X, K, M, V, a, b, 600) #infers z
+Y_pred = predict(Z, K, M)
 np.savetxt("y.txt",Y, fmt='%d')
 np.savetxt("y_pred.txt", Y_pred, fmt='%d')
