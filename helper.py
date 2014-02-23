1a1f405cd4c0e337cd1917dd41e0be46d3273dd7
vineel <vineelkpratap@gmail.com>
2014-02-23 03:21:13 -0800

    Theta plot & Top words

--- helper.py
+++ helper.py
@@ -1,7 +1,10 @@
 import numpy as np
 from scipy import io as sio
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 
-def perform_lda(X, K, M, V, alpha, beta, num_epochs):
+
+def perform_lda(X, K, M, V, alpha, beta, num_epochs, vocab, num):
 	
 ############ initialisation ###########################################	
 	
@@ -16,7 +19,7 @@ def perform_lda(X, K, M, V, alpha, beta, num_epochs):
 		doc.append([nz[temp] for temp in range(nz.size) for temp2 in range(int(X[m,nz[temp]]))])
 		for i, w_i in enumerate(doc[m]):
 			z = Z_doc[i]
-			q[z,w_i],n[m,z] = q[z,w_i]+1,n[m,z]+1
+			q[z,w_i], n[m,z] = q[z,w_i]+1, n[m,z]+1
 		Z.append(Z_doc)
 	Q = np.sum(q,axis=1) + np.sum(beta) #denominator term
 	p = np.empty(K, dtype=float)
@@ -36,7 +39,7 @@ def perform_lda(X, K, M, V, alpha, beta, num_epochs):
 				q[z,w_i], n[m,z], Q[z] = q[z,w_i]-1, n[m,z]-1, Q[z]-1
 				
 				# sampling topic new_z for w_i
-				p = q[:,w_i] * n[m,:]  / Q
+				p = q[:,w_i] * n[m,:] / Q
 				new_z = np.random.multinomial(1, p / p.sum()).argmax()
 				
 				# set z the new topic and increment counters
@@ -44,16 +47,30 @@ def perform_lda(X, K, M, V, alpha, beta, num_epochs):
 				q[new_z,w_i], n[m,new_z], Q[new_z] = q[new_z,w_i]+1, n[m,new_z]+1, Q[new_z]+1
 				
 		it+=1
-	print "lda finished ..."
+		
+##################printing top_words##########################
+	for i in range(K):
+		print i , vocab[q[i,:].argsort()[-num:]]
+
+	
 	return Z
 
 
 def predict(Z, K, M):
-	count = np.empty(K, dtype=int)
-	y = np.empty(len(Z), dtype=int)
-	compare = [1..K]
+	theta = np.empty((M,K))
+	y = np.empty(M,dtype=int)
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection='3d')
+	color = 'rgb'
 	for m in range(M):
 		for j in range(K):
-			count[j] = np.sum(Z[m] == j)
-		y[m] = np.argmax(count) + 1
+			theta[m,j] = np.sum(Z[m] == j)
+		theta[m,:] = theta[m,:] / np.sum(theta[m,:])
+		y[m] = np.argmax(theta[m,:]) + 1
+		ax.scatter(theta[m,0], theta[m,1], theta[m,2], c=color[y[m]-1])	########plotting theta grapha
+	plt.show()
 	return y
+
+
+	
+	
--- main.py
+++ main.py
@@ -7,11 +7,10 @@ X = f['classic400'] #scipy.sparse.csc_matrix
 
 
 K,(M,V) = 3,X.shape #no. of topics
-a = (50.0/K)*np.ones(K) #alpha - parameter of topics prio
+a = (20.0/K)*np.ones(K) #alpha - parameter of topics prio
 b = 0.1*np.ones(V) #beta -  parameter of words prior
 
-
-Z = perform_lda(X, K, M, V, a, b, 600) #infers z
+Z = perform_lda(X, K, M, V, a, b, 2000, vocab, 10) #infers z
 Y_pred = predict(Z, K, M)
 np.savetxt("y.txt",Y, fmt='%d')
 np.savetxt("y_pred.txt", Y_pred, fmt='%d')
